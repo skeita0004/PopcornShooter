8139fd1d31f0f1974a54cb4074a757eedc1535a6
啓大 齋藤 <240004@jc-21.jp>
2025-12-11 13:37:49 +0900

    VirtualCameraクラス追加

---
+++ Source/Engine/VirtualCamera.cpp
@@ -0,0 +1,92 @@
+﻿#include "VirtualCamera.hpp"
+#include "Screen.hpp"
+#include <windows.h>
+
+VirtualCamera::VirtualCamera(const XMFLOAT3& _upDir,
+                             const float     _fovAngleDeg,
+                             const float     _nearClip,
+                             const float     _farClip) :
+                             upDir_      (_upDir),
+                             fovAngleDeg_(_fovAngleDeg),
+                             nearClip_   (_nearClip),
+                             farClip_    (_farClip)
+{
+}
+
+void VirtualCamera::Init()
+{
+    matProjection_ = XMMatrixIdentity();
+    matView_       = XMMatrixIdentity();
+    matBillboard_  = XMMatrixIdentity();
+
+    nearClip_ = 0.1f;
+    farClip_  = 1000000.0f;
+}
+
+void VirtualCamera::Update()
+{
+    matProjection_ = XMMatrixPerspectiveFovLH(XMConvertToRadians(fovAngleDeg_),
+                                              Screen::GetAspectRatio(),
+                                              nearClip_,
+                                              farClip_);
+
+    matView_       = XMMatrixLookAtLH(XMLoadFloat3(&transform_.position),
+                                      XMLoadFloat3(&targetTransform_.position),
+                                      XMLoadFloat3(&upDir_));
+
+    //ビルボード行列
+    //（常にカメラの方を向くように回転させる行列。パーティクルでしか使わない）
+    //http://marupeke296.com/DXG_No11_ComeOnBillboard.html
+    matBillboard_ = XMMatrixLookAtLH(XMVectorSet(0, 0, 0, 0),
+                                     XMLoadFloat3(&targetTransform_.position) - XMLoadFloat3(&transform_.position),
+                                     XMLoadFloat3(&upDir_));
+    matBillboard_ = XMMatrixInverse(nullptr, matBillboard_);
+    //ImGui::Begin("CamPos");
+
+    //ImGui::InputFloat("X: ", &_position.x);
+    //ImGui::InputFloat("Y: ", &_position.y);
+    //ImGui::InputFloat("Z: ", &_position.z);
+
+    //ImGui::End();
+
+}
+
+Transform& VirtualCamera::GetTransform()
+{
+    return transform_;
+}
+
+Transform& VirtualCamera::GetTargetTransform()
+{
+    return targetTransform_;
+}
+
+float& VirtualCamera::GetFovAngleDeg()
+{
+    return fovAngleDeg_;
+}
+
+float& VirtualCamera::GetNearClip()
+{
+    return nearClip_;
+}
+
+float& VirtualCamera::GetFarClip()
+{
+    return farClip_;
+}
+
+const XMMATRIX& VirtualCamera::GetProjectionMat() const
+{
+    return matProjection_;
+}
+
+const XMMATRIX& VirtualCamera::GetViewMat() const
+{
+    return matView_;
+}
+
+const XMMATRIX& VirtualCamera::GetBillboardMat() const
+{
+    return matBillboard_;
+}
---
+++ Source/Engine/VirtualCamera.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "Transform.hpp"
+
+class VirtualCamera
+{
+public:
+    VirtualCamera(const XMFLOAT3&  _upDir,
+                  const float      _fovAngleDeg,
+                  const float      _nearClip,
+                  const float      _farClip);
+
+    void Init();
+    void Update();
+
+    Transform& GetTransform();
+    Transform& GetTargetTransform();
+    float& GetFovAngleDeg();
+    float& GetNearClip();
+    float& GetFarClip();
+    const XMMATRIX& GetProjectionMat() const;
+    const XMMATRIX& GetViewMat() const;
+    const XMMATRIX& GetBillboardMat() const;
+
+private:
+    Transform transform_;
+    Transform targetTransform_;
+    XMFLOAT3  upDir_;
+    float     fovAngleDeg_;
+    float     nearClip_;
+    float     farClip_;
+    XMMATRIX  matProjection_;
+    XMMATRIX  matView_;
+    XMMATRIX  matBillboard_;
+
+    inline static const float DEFAULT_NEAR_CLIP{ 0.1f };
+    inline static const float DEFAULT_FAR_CLIP{1000000.f};
+};
