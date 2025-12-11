d2cfd1f300a554afcab237eb2cb9880b585c5d98
啓大 齋藤 <240004@jc-21.jp>
2025-12-11 13:37:38 +0900

    CameraSetクラス追加

---
+++ Source/Engine/CameraSet.cpp
@@ -0,0 +1,55 @@
+﻿#include "CameraSet.hpp"
+#include "VirtualCamera.hpp"
+#include "SafeCleaning.hpp"
+
+CameraSet::CameraSet()
+{
+}
+
+void CameraSet::Init()
+{
+    instances_.push_back(new VirtualCamera(XMFLOAT3(0, 1, 0),
+                                           120.f,
+                                           0.1f,
+                                           1000000.f));
+    instances_[hCurrent_]->Init();
+}
+
+void CameraSet::Release()
+{
+    for (auto& instance : instances_)
+    {
+        SafeCleaning::SafeDelete(instance);
+    }
+    instances_.clear();
+}
+
+int CameraSet::Create(const DirectX::XMFLOAT3& _upDir,
+                      const float _fovAngleDeg,
+                      const float _nearClip,
+                      const float _farClip)
+{
+    instances_.push_back(new VirtualCamera(_upDir,
+                                           _fovAngleDeg,
+                                           _nearClip,
+                                           _farClip));
+    int handle = instances_.size() - 1;
+    instances_[handle]->Init();
+
+    return handle;
+}
+
+Transform& CameraSet::GetTransform()
+{
+    return instances_[hCurrent_]->GetTransform();
+}
+
+void CameraSet::SetCurrent(int _hCam)
+{
+    hCurrent_ = _hCam;
+}
+
+VirtualCamera* CameraSet::GetCurrent()
+{
+    return instances_[hCurrent_];
+}
---
+++ Source/Engine/CameraSet.hpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <vector>
+#include <DirectXMath.h>
+#include "VirtualCamera.hpp"
+
+class Transform;
+
+class CameraSet
+{
+public:
+    CameraSet();
+
+    void Init();
+    void Release();
+
+    /// @brief カメラを作成する
+    /// @note  作成してすぐにCurrentとして
+    /// 登録されるわけではない。
+    /// @return 作成されたカメラのハンドル
+    int Create(const DirectX::XMFLOAT3& _upDir,
+               const float              _fovAngleDeg,
+               const float              _nearClip,
+               const float              _farClip);
+
+    /// @brief Currentとして設定されている
+    /// カメラのTransformを返す
+    /// @return カメラのTransform
+    Transform& GetTransform();
+
+    /// @brief 指定したカメラをCurrentにする
+    /// @param _hCam カメラのハンドル
+    void SetCurrent(int _hCam);
+
+    /// @brief Currentとして設定されているカメラの
+    /// 参照を返す
+    /// @return VirtualCamera&
+    VirtualCamera* GetCurrent();
+
+private:
+    inline static int hCurrent_{0};
+    static std::vector<VirtualCamera*> instances_;
+};
